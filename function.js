5d7e8ff7173312ef95fb1f8ae4a576c52c2531e9
WomenCanCode2022 <112905215+WomenCanCode2022@users.noreply.github.com>
2022-09-09 22:22:41 -0300

    Update function.js

--- function.js
+++ function.js
@@ -1,3 +1,17 @@
+fCalcularImc = (peso, altura) => {
+    return peso/(altura*altura)
+};
+
+fClassificacao = (imc) => {
+    if (imc < 18.5) {
+        return 'Voce esta abaixo do peso';
+    } else if (imc < 24.9) {
+        return 'Voce esta no peso ideal';
+    } else {
+        return 'Voce esta com sobrepeso';
+    }
+    };
+
 module.exports = async function (context, req) {
     let nomeUsuario = String(req.query.nome);
     let pesoUsuario = Number(req.query.peso);
@@ -6,20 +20,6 @@ module.exports = async function (context, req) {
     if (isNaN(pesoUsuario) || isNaN(alturaUsuario)) {
         return context.res.status(400).send('Formato de dados incorreto, o campo peso e altura aceitam somente numeros.');
     }
-
-    fCalcularImc = (peso, altura) => {
-        return peso/(altura*altura)
-    };
-
-    fClassificacao = (imc) => {
-        if (imc < 18.5) {
-            return 'Voce esta abaixo do peso';
-        } else if (imc < 24.9) {
-            return 'Voce esta no peso ideal';
-        } else {
-            return 'Voce esta com sobrepeso';
-        }
-    };
 
     let imcUsuario = fCalcularImc(pesoUsuario, alturaUsuario);
     let classificacaoUsuario = fClassificacao(imcUsuario);
