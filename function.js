89e78f09252ddee59e7cdd7c5167f9d7edf0267a
WomenCanCode2022 <112905215+WomenCanCode2022@users.noreply.github.com>
2022-09-09 22:22:04 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,34 @@
+module.exports = async function (context, req) {
+    let nomeUsuario = String(req.query.nome);
+    let pesoUsuario = Number(req.query.peso);
+    let alturaUsuario = Number(req.query.altura);
+
+    if (isNaN(pesoUsuario) || isNaN(alturaUsuario)) {
+        return context.res.status(400).send('Formato de dados incorreto, o campo peso e altura aceitam somente numeros.');
+    }
+
+    fCalcularImc = (peso, altura) => {
+        return peso/(altura*altura)
+    };
+
+    fClassificacao = (imc) => {
+        if (imc < 18.5) {
+            return 'Voce esta abaixo do peso';
+        } else if (imc < 24.9) {
+            return 'Voce esta no peso ideal';
+        } else {
+            return 'Voce esta com sobrepeso';
+        }
+    };
+
+    let imcUsuario = fCalcularImc(pesoUsuario, alturaUsuario);
+    let classificacaoUsuario = fClassificacao(imcUsuario);
+
+    context.res.json({
+        nome: nomeUsuario, 
+        peso: pesoUsuario, 
+        altura: alturaUsuario, 
+        imc: imcUsuario, 
+        classificacao: classificacaoUsuario
+    });
+}
